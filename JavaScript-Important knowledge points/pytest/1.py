cea45543b275e26e8084ec429d4378a9c57af789
jinxinfeng <33054241+jinxinfeng@users.noreply.github.com>
2021-08-09 16:50:22 +0800

    Update 1.py
    123

--- JavaScript-Important knowledge points/pytest/1.py
+++ JavaScript-Important knowledge points/pytest/1.py
@@ -81,7 +81,85 @@ class Cart(Customer,Good):
                     return self.maxDiscount
             
     
+########
+class Customer:
+    def _init_(self,rank:float, num:int, order:list, money:float,discount:float)->None:
+        #用户积分
+        self.rank=rank
+        #用户的初始订单数
+        self.num=0
+        #用户的订单列表
+        self.order=[]
+        #未打折的金额
+        self.money=money
+        #未打折就是1
+        self.discount=1
+    def setRank(self,rank:float)->None:
+        self.rank=rank
+    def getRanke(self)->float:
+        return self.rank
+    def setMoney(self,money:float)->None:
+        self.money=money
+    def getMoney(self)->float:
+        return self.money
+    def setNum(self,num:int)->None:
+        self.num=num
+    def getNum(self)->None:
+        return self.num
+    def setOrder(self,order:list)->None:
+        self.order=order
+    def getOrder(self)->list:
+        return self.order
+    def setDiscount(self, discount:float)-> None:
+        self.discount=discount
+    def getDisocunt(self, discount)->float:
+        return self.discount
+    def orderDiscount1(self)->float:
+        if self.rank>=1000:
+            self.setDiscount(0.95)
+        else:
+            self.setDiscount(1)
+        return self.discount
 
+    def orderDiscount2(self)->float:
+        dict={}
+        list=[]
+        for o in self.getOrder():
+            dict[o]=dict.get(o,0)+1
+        for v in dict.values():
+            list.append(v)
+        a=max(list)
+        if a>=20:
+            self.setDiscount(0.9)
+        else:
+            self.setDiscount(1)  
+        return self.discount
+    def orderDiscount3(self)->float:
+        dict={}
+        for o in self.getOrder():
+            dict[o]=dict.get(o,0)+1
+        if len(dict)>=10:
+            self.setDiscount(0.93)
+        else:
+            self.setDiscount(1)
+        return self.discount
+ #输入用户数据
+if __name__ == "__main__":
+    customer=Customer()
+    order=["青菜","西红柿","辣椒","哈密瓜","香蕉","青菜","西红柿","青菜"]
+    customer.setOrder(order)
+    customer.setMoney(100)
+    customer.setRank(2000)
+    discount1=customer.orderDiscount1()
+    discount2=customer.orderDiscount2()
+    discount3=customer.orderDiscount3()
+    list=[discount1,discount2,discount3]
+    print(list)
+    a=min(list)
+    b=a*customer.getMoney()
+    remainrank=customer.getMoney()*(1-a)
+    customer.setRank(customer.rank-remainrank)
+    print(customer.rank)
 
   
        
